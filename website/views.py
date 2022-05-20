1e01f9529202c1bcbbe231ab44115bc6ed5a61d6
iamkidee <enochjesse884@gmail.com>
2022-05-20 22:27:37 +0100

    final commit for now

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template, request,flash
 from flask_login import login_required,current_user
+from .models import Note
+from website.__inti__ import db
+import json
 """
 The function home() returns the string "Test" when the user visits the root of the website
     :return: A string
@@ -7,13 +10,39 @@ The function home() returns the string "Test" when the user visits the root of t
 
 views = Blueprint('views',__name__,template_folder='/website/templates')
 
-@views.route('/Home')
+@views.route('/Home',methods=['GET'])
 @login_required
 def home():
     return render_template("home.html", user=current_user)
-@views.route('/main') 
-def main():
-    return render_template("Notes.html")
+
+
+@views.route('/Note', methods=['POST','GET'])
+@login_required 
+def note():
+    if request.method == 'POST':
+        note =request.form.get('note')
+        if len(note)< 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note= Note(data=note,user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note has been saved ', category='success')
+    return render_template("Notes.html",user=current_user)
+
+
 @views.route('/')
 def landing():
     return render_template("base.html")
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():	
+    note =json.loads(request.data)
+    noteId =note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            
+    return jsonify({})
